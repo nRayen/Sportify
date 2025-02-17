89fa5542596ba698bc2f8a684eded2c0e0adba74
nRayen <rayen.ndiaye@gmail.com>
2025-02-18 00:14:20 +0100

    (main) : started dashboard streak card

--- src/app/(app)/dashboard/page.jsx
+++ src/app/(app)/dashboard/page.jsx
@@ -1,18 +1,38 @@
+import { getUser } from "@/libs/dal";
+import capitalize from "@/utils/Capitalize";
 import React from "react";
 
-const PageDashboard = () => {
+const PageDashboard = async () => {
+
+	const user = await getUser()
+	const date = new Date().toLocaleDateString("fr-FR", {
+		weekday: "long",   // Jour en toutes lettres (ex : lundi)
+		day: "2-digit",    // Jour sur 2 chiffres (ex : 02)
+		month: "long",
+	  }).split(' ');
+
+	const streak = 7
 
 
 	return (
 		<div className="h-full w-full gap-8 grid sm:grid-cols-2 p-8 grid-cols-1">
 
-			<section className="p-4">
-				<h2>Prochaines séances</h2>
-
+			<section className="flex flex-col max-w-1/2 grow-0">
 				<div>
-					<p>Lundi 06 Mai</p>
+					<h2 className="text-4xl">Bienvenue <span className="text-primary tracking-wide text-5xl">{user.firstname}</span> </h2>
+					<p className="text-lg">{capitalize(date[0])} <span className="text-primary">{date[1]}</span> {capitalize(date[2])}</p>
+				</div>
+
+				<div className="flex items-center">
+					<p>Streak en cours :</p>
+					<p className="text-9xl ml-auto">{streak}</p>
 				</div>
 			</section>
+
+
+
+
+
 
 
       {/* Planning */}
